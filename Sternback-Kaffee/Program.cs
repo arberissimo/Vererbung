7a41eafdcf577a88d06f06695253d857676c0648
Arber Alaj <Arber.Alaj@ksb-sg.ch>
2019-10-01 10:34:08 +0200

    Add project files.

---
+++ Sternback-Kaffee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sternback_Kaffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
